276c39df8b1da0117dae5fd615b68bfe30d63975
DonOmalVindula <omalvindula@live.com>
2025-06-20 17:47:23 +0530

    Add API methods for role sharing with organizations

--- features/admin.organizations.v1/models/organizations.ts
+++ features/admin.organizations.v1/models/organizations.ts
@@ -149,3 +149,7 @@ export interface GetOrganizationsParamsInterface {
     recursive?: boolean;
     isRoot?: boolean;
 }
+
+export interface SelectedOrganizationRoleInterface extends OrganizationRoleInterface {
+    selected: boolean;
+}

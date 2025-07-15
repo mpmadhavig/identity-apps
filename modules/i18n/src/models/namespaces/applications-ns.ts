a156154d0d2758a7f9ebbeb82a2ee73f27872d72
DonOmalVindula <omalvindula@live.com>
2025-07-15 10:09:20 +0530

    Fix i18n

--- modules/i18n/src/models/namespaces/applications-ns.ts
+++ modules/i18n/src/models/namespaces/applications-ns.ts
@@ -776,7 +776,7 @@ export interface ApplicationsNS {
                         };
                     };
                 };
-            };allRolesAndOrgsSharingMessage
+            };
             sharedAccess: {
                 allRolesAndOrgsSharingMessage: string;
                 subTitle: string;
